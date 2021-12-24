8ed2e7cb40bc84b46b170034c84737e9bbc93ced
Mingxuan Zhang <mingxuan.academic@gmail.com>
2021-12-24 15:53:53 +0800

    met some issues while training. Found the error is in load_data.py. Therefore load_data.py has been updated with new data loading method.

--- load_data.py
+++ load_data.py
@@ -1,49 +1,48 @@
-import logging
 
-import torch.nn.functional as F
+from torchvision import transforms as T
+from PIL import Image
+from torch.utils import data
+import os, random
 
-from torchvision import transforms, datasets
-from torch.utils.data import DataLoader, RandomSampler, DistributedSampler, SequentialSampler
+random.seed(1)
 
-logger = logging.getLogger(__name__)
-traindir = '../data/imgs/train'  # your custom dataset path
-testdir = '../data/imgs/test'
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+def get_filepath(dir_root):
+    # get root path of the files and store them in list file_paths
+    file_paths = []
+    for root, dirs, files in os.walk(dir_root):
+        for file in files:
+            file_paths.append(os.path.join(root, file))
+    return file_paths
 
+class DriverDataset(data.Dataset):
 
-def get_loader(args):
-    trainset = datasets.ImageFolder(
-        traindir,
-        transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ])
-    )
+    def __init__(self, data_root, args, transforms=None, train=True):
+        self.train = train
+        imgs_in = get_filepath(data_root)
+        random.shuffle(imgs_in)
+        imgs_num = len(imgs_in)
 
-    testset = datasets.ImageFolder(
-        testdir,
-        transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ])
-    )
+        if transforms is None and self.train:
+            self.transforms = T.Compose([T.RandomHorizontalFlip(),
+                                         T.RandomResizedCrop(224),
+                                         T.ToTensor()])
+        else:
+            self.transforms = T.Compose([T.Resize(size=(224, 224)),
+                                         T.ToTensor()])
+        if self.train is True: # train set
+            self.imgs = imgs_in[:int(args.traindata_size * imgs_num)]
+            # print('test') # for debug.
+        elif self.train is False:          # validation set
+            self.imgs = imgs_in[int(args.valdata_size * imgs_num):]
+            # print('test') # for debug
 
+    def __getitem__(self, index):
+        img_path = self.imgs[index]
 
-    train_sampler = RandomSampler(trainset)
-    test_sampler = SequentialSampler(testset)
-    train_loader = DataLoader(trainset,
-                              sampler=train_sampler,
-                              batch_size=args.train_batch_size,
-                              num_workers=0,
-                              pin_memory=True)
-    test_loader = DataLoader(testset,
-                             sampler=test_sampler,
-                             batch_size=args.eval_batch_size,
-                             num_workers=0,
-                             pin_memory=True) if testset is not None else None
+        label = int(img_path.split('/')[-2][1])
+        data = Image.open(img_path)
+        data = self.transforms(data)
+        return data, label
 
-    return train_loader, test_loader
+    def __len__(self):
+        return len(self.imgs)
